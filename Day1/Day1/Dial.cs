9482028d08f145ad63ee2c75603c302bcb77ae16
JLester <ScorpionJL@hotmail.com>
2025-12-08 17:32:05 -0500

    Part 1 sample solution

---
+++ Day1/Day1/Dial.cs
@@ -0,0 +1,17 @@
+﻿namespace AOC25.Day1;
+
+public class Dial
+{
+    public Dial() { }
+    public Dial(int value) => Rotate(value);
+
+
+    public int Value { get; private set; }
+
+
+    public void Rotate(int amount) => Value = ((Value + amount) % 100 + 100) % 100;
+
+    public void RotateRight(int amount) => Rotate(amount);
+
+    public void RotateLeft(int amount) => Rotate(-amount);
+}
---
+++ Day1/Day1/Solution.cs
@@ -0,0 +1,32 @@
+﻿namespace AOC25.Day1;
+
+public class Solution
+{
+    public static int Part1(IPuzzleInput puzzleInput)
+    {
+        var result = 0;
+        var dial = new Dial(50);
+
+        foreach (var line in puzzleInput.GetInput())
+        {
+            if (line.StartsWith('R'))
+            {
+                if (int.TryParse(line[1..], out var parsedVal)) { dial.RotateRight(parsedVal); }
+                LogRotation(dial, parsedVal);
+                if (dial.Value == 0) { result++; }
+            }
+            else if (line.StartsWith('L'))
+            {
+                if (int.TryParse(line[1..], out var parsedVal)) {  dial.RotateLeft(parsedVal); }
+                Console.WriteLine($"Dial changed by {-parsedVal}, new position: {dial.Value}");
+                if (dial.Value == 0) { result++; }
+            }
+        }
+        return result;
+
+        static void LogRotation(Dial dial, int parsedVal)
+        {
+            Console.WriteLine($"Dial changed by {parsedVal}, new position: {dial.Value}");
+        }
+    }
+}
