06d6d4e7fd7f1ae1a5b7d6f3e45eeeae4abaeabd
JLester <ScorpionJL@users.noreply.github.com>
2025-12-09 22:31:21 -0500

    Day 1 Part 1 WIP (#3)

--- Day1/Day1/Solution.cs
+++ Day1/Day1/Solution.cs
@@ -2,31 +2,46 @@
 
 public class Solution
 {
+    private static int RotationAmount(string line)
+    {
+        if (line.Length < 2) { return 0; }
+        var direction = char.ToUpper(line[0]);
+        int rotation = int.TryParse(line[1..], out var parsed) ? parsed : 0;
+        return (direction, rotation) switch
+        {
+            ('R', _) => rotation,
+            ('L', _) => -rotation,
+            _ => 0,
+        };
+    }
+
     public static int Part1(IPuzzleInput puzzleInput)
     {
-        var result = 0;
-        var dial = new Dial(50);
+        int zeros = 0;
+        var dial = new DialPosition(50);
 
+
+        // foreach line in input
         foreach (var line in puzzleInput.GetInput())
         {
-            if (line.StartsWith('R'))
-            {
-                if (int.TryParse(line[1..], out var parsedVal)) { dial.RotateRight(parsedVal); }
-                LogRotation(dial, parsedVal);
-                if (dial.Value == 0) { result++; }
-            }
-            else if (line.StartsWith('L'))
-            {
-                if (int.TryParse(line[1..], out var parsedVal)) {  dial.RotateLeft(parsedVal); }
-                Console.WriteLine($"Dial changed by {-parsedVal}, new position: {dial.Value}");
-                if (dial.Value == 0) { result++; }
-            }
-        }
-        return result;
+            // parse line to get rotation amount
+            var rotation = RotationAmount(line);
+            if (rotation == 0) { continue; }
 
-        static void LogRotation(Dial dial, int parsedVal)
-        {
-            Console.WriteLine($"Dial changed by {parsedVal}, new position: {dial.Value}");
+            // if line is valid, rotate dial and new dial position
+            dial = dial.RotateDial(rotation);
+            LogRotation(dial, rotation);
+
+            // if dial is at 0, increment zeros
+            if (dial.Value == 0) { zeros++; }
         }
+
+        // return number of times dial was at 0
+        return zeros;
+    }
+
+    static void LogRotation(DialPosition dial, int parsedVal)
+    {
+        Console.WriteLine($"Dial changed by {parsedVal}, new position: {dial.Value}");
     }
 }
