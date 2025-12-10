4110b6163e0da10a31fcd4eece9579d1efda5bc9
JLester <ScorpionJL@hotmail.com>
2025-12-09 22:38:22 -0500

    Updated unit tests

--- Day1.Tests/Day1/DialTests.cs
+++ Day1.Tests/Day1/DialTests.cs
@@ -8,77 +8,77 @@ public class DialTests
     [Fact]
     public void ConstructorWithoutParametersCreatesDialAtZero()
     {
-        var dial = new Dial();
-        
-        Assert.Equal(0, dial.Position.Value);
+        var dial = new DialPosition(0);
+
+        Assert.Equal(0, dial.Value);
     }
 
     [Fact]
     public void ConstructorWithValueSetsInitialPosition()
     {
-        var dial = new Dial(50);
-        
-        Assert.Equal(50, dial.Position.Value);
+        var dial = new DialPosition(50);
+
+        Assert.Equal(50, dial.Value);
     }
 
     [Fact]
     public void RotatePositiveAmountIncreasesValue()
     {
-        var dial = new Dial(10);
-        
-        dial.Rotate(5);
-        
-        Assert.Equal(15, dial.Position.Value);
+        var dial = new DialPosition(10);
+
+        dial = dial.RotateDial(5);
+
+        Assert.Equal(15, dial.Value);
     }
 
     [Fact]
     public void RotateNegativeAmountDecreasesValue()
     {
-        var dial = new Dial(20);
-        
-        dial.Rotate(-5);
-        
-        Assert.Equal(15, dial.Position.Value);
+        var dial = new DialPosition(20);
+
+        dial = dial.RotateDial(-5);
+
+        Assert.Equal(15, dial.Value);
     }
 
     [Fact]
     public void RotateWrapsAroundAt100()
     {
-        var dial = new Dial(95);
-        
-        dial.Rotate(10);
-        
-        Assert.Equal(5, dial.Position.Value);
+        var dial = new DialPosition(95);
+
+        dial = dial.RotateDial(10);
+
+        Assert.Equal(5, dial.Value);
     }
 
     [Fact]
     public void RotateWrapsAroundAtZeroWhenNegative()
     {
-        var dial = new Dial(5);
-        
-        dial.Rotate(-10);
-        
-        Assert.Equal(95, dial.Position.Value);
+        var dial = new DialPosition(5);
+
+        dial = dial.RotateDial(-10);
+
+        Assert.Equal(95, dial.Value);
     }
 
     [Fact]
     public void RotateLargePositiveAmountWrapsCorrectly()
     {
-        var dial = new Dial(50);
-        
-        dial.Rotate(200);
-        
-        Assert.Equal(50, dial.Position.Value);
+        var dial = new DialPosition(50);
+
+        dial = dial.RotateDial(200);
+
+        Assert.Equal(50, dial.Value);
     }
 
     [Fact]
     public void RotateLargeNegativeAmountWrapsCorrectly()
     {
-        var dial = new Dial(50);
-        
-        dial.Rotate(-200);
-        
-        Assert.Equal(50, dial.Position.Value);
+        var dial = new DialPosition(50);
+
+        dial = dial.RotateDial(-200);
+
+        Assert.Equal(50, dial.Value);
     }
 
     [Theory]
@@ -89,83 +89,83 @@ public class DialTests
     [InlineData(75, 50, 25)]
     public void RotateWithVariousAmountsProducesCorrectValue(int start, int amount, int expected)
     {
-        var dial = new Dial(start);
-        
-        dial.Rotate(amount);
-        
-        Assert.Equal(expected, dial.Position.Value);
+        var dial = new DialPosition(start);
+
+        dial = dial.RotateDial(amount);
+
+        Assert.Equal(expected, dial.Value);
     }
 
     [Fact]
     public void RotateRightIncreasesValue()
     {
-        var dial = new Dial(10);
-        
-        dial.RotateRight(5);
-        
-        Assert.Equal(15, dial.Position.Value);
+        var dial = new DialPosition(10);
+
+        dial = dial.RotateDial(5);
+
+        Assert.Equal(15, dial.Value);
     }
 
     [Fact]
     public void RotateRightWrapsAt100()
     {
-        var dial = new Dial(95);
-        
-        dial.RotateRight(10);
-        
-        Assert.Equal(5, dial.Position.Value);
+        var dial = new DialPosition(95);
+
+        dial = dial.RotateDial(10);
+
+        Assert.Equal(5, dial.Value);
     }
 
     [Fact]
     public void RotateLeftDecreasesValue()
     {
-        var dial = new Dial(20);
-        
-        dial.RotateLeft(5);
-        
-        Assert.Equal(15, dial.Position.Value);
+        var dial = new DialPosition(20);
+
+        dial = dial.RotateDial(-5);
+
+        Assert.Equal(15, dial.Value);
     }
 
     [Fact]
     public void RotateLeftWrapsAtZero()
     {
-        var dial = new Dial(5);
-        
-        dial.RotateLeft(10);
-        
-        Assert.Equal(95, dial.Position.Value);
+        var dial = new DialPosition(5);
+
+        dial = dial.RotateDial(-10);
+
+        Assert.Equal(95, dial.Value);
     }
 
     [Fact]
     public void MultipleRotationsAccumulate()
     {
-        var dial = new Dial(50);
-        
-        dial.Rotate(10);
-        dial.Rotate(-5);
-        dial.Rotate(20);
-        
-        Assert.Equal(75, dial.Position.Value);
+        var dial = new DialPosition(50);
+
+        dial = dial.RotateDial(10);
+        dial = dial.RotateDial(-5);
+        dial = dial.RotateDial(20);
+
+        Assert.Equal(75, dial.Value);
     }
 
     [Fact]
     public void RotateRightWithNegativeAmountDecreasesValue()
     {
-        var dial = new Dial(20);
-        
-        dial.RotateRight(-5);
-        
-        Assert.Equal(15, dial.Position.Value);
+        var dial = new DialPosition(20);
+
+        dial = dial.RotateDial(-5);
+
+        Assert.Equal(15, dial.Value);
     }
 
     [Fact]
     public void RotateLeftWithNegativeAmountIncreasesValue()
     {
-        var dial = new Dial(10);
-        
-        dial.RotateLeft(-5);
-        
-        Assert.Equal(15, dial.Position.Value);
+        var dial = new DialPosition(10);
+
+        dial = dial.RotateDial(5);
+
+        Assert.Equal(15, dial.Value);
     }
 
     [Theory]
@@ -181,24 +181,24 @@ public class DialTests
     [InlineData(-82)]
     public void RotateHandlesInputFromProgram(int amount)
     {
-        var dial = new Dial(50);
-        
-        dial.Rotate(amount);
-        
-        Assert.InRange(dial.Position.Value, 0, 99);
+        var dial = new DialPosition(50);
+
+        dial = dial.RotateDial(amount);
+
+        Assert.InRange(dial.Value, 0, 99);
     }
 
     [Fact]
     public void WhenSequenceFromProgramAppliedThenFinalPositionIsCorrect()
     {
-        List<int> input = [-68, -30, 48, -5, 60, -55, -1, -99, 14, -82];
-        var dial = new Dial(50);
-        
+        var input = new[] { -68, -30, 48, -5, 60, -55, -1, -99, 14, -82 };
+        var dial = new DialPosition(50);
+
         foreach (var change in input)
         {
-            dial.Rotate(change);
+            dial = dial.RotateDial(change);
         }
-        
-        Assert.Equal(32, dial.Position.Value);
+
+        Assert.Equal(32, dial.Value);
     }
 }
