d38f6f778a16d0b11840175b3588b80b270206d6
JLester <ScorpionJL@hotmail.com>
2025-12-16 22:11:44 -0500

    setup

---
+++ Day1/Day3/Puzzle.cs
@@ -0,0 +1,12 @@
+﻿namespace AOC25.Day3;
+
+public static class Puzzle
+{
+    public static (long part1, long part2) Solve(IPuzzleInput puzzleInput)
+    {
+        var part1 = 0L;
+        var part2 = 0L;
+
+        return (part1, part2);
+    }
+}
--- Day1/Program.cs
+++ Day1/Program.cs
@@ -1,8 +1,11 @@
 ﻿using AOC25;
 using Day1 = AOC25.Day1;
 using Day2 = AOC25.Day2;
+using Day3 = AOC25.Day3;
 
-SolveDay2();
+//SolveDay1();
+//SolveDay2();
+SolveDay3();
 
 static void SolveDay1()
 {
@@ -18,6 +21,16 @@ static void SolveDay2()
 {
     IPuzzleInput input = PuzzleInput.Create(2);
     var (part1, part2) = Day2.Puzzle.Solve(input);
+    Console.WriteLine($"""
+    Part 1:  {part1}
+    Part 2:  {part2}
+    """);
+}
+
+static void SolveDay3()
+{
+    IPuzzleInput input = PuzzleInput.Create(3, true);
+    var (part1, part2) = Day3.Puzzle.Solve(input);
     Console.WriteLine($"""
     Part 1:  {part1}
     Part 2:  {part2}
