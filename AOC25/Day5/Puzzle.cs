9e57ad54e3bf9ed21e0bfc7d3bb1aff8b2986f06
JLester <ScorpionJL@users.noreply.github.com>
2025-12-28 15:13:38 -0500

    Day 5 solution (#8)

---
+++ AOC25/Day5/Puzzle.cs
@@ -0,0 +1,90 @@
+﻿using System.Numerics;
+using AOC25.Common;
+
+namespace AOC25.Day5;
+
+public static class Puzzle
+{
+    public static (long part1, long part2) Solve(IPuzzleInput puzzleInput)
+    {
+        var (ranges, ids) = puzzleInput.ParseInput();
+
+        // find the count of ids that fall within any of the ranges
+        long part1 = ids.Count(id => IsFreshIngredient(id, ranges));
+        long part2 = ranges.MergeOverlappingRanges().Sum(range => range.RangeSize);
+
+        return (part1, part2);
+    }
+
+    private static bool IsFreshIngredient(long ingredient, HashSet<NumericRange<long>> ranges) => 
+        ranges.Any(range => range.IsInRange(ingredient));
+
+    private static IEnumerable<NumericRange<long>> MergeOverlappingRanges(this HashSet<NumericRange<long>> ranges)
+    {
+        var sorted = ranges.ToSortedList();
+        var current = sorted[0];
+        foreach (var range in sorted.Skip(1))
+        {
+            if (range.Overlaps(current)) { current = ExtendRange(current, range); }
+            else { yield return current; current = range; }
+        }
+        yield return current; // yield last working range
+
+        static NumericRange<long> ExtendRange(NumericRange<long> current, NumericRange<long> range) =>
+            new(Math.Min(current.Start, range.Start), Math.Max(current.End, range.End));
+    }
+
+
+    private static (HashSet<NumericRange<long>> ranges, HashSet<long> ids) ParseInput(this IPuzzleInput input)
+    {
+        using var enumerator = input.ReadInput().GetEnumerator();
+
+        var ranges = enumerator.ForEachRange()
+            .Where(range => !range.Equals(new(0, 0)))
+            .ToHashSet();
+
+        var ids = enumerator.ForEachId().ToHashSet();
+
+        return (ranges, ids);
+    }
+
+    extension(IEnumerator<string> enumerator)
+    {
+        private IEnumerable<NumericRange<long>> ForEachRange()
+        {
+            while (enumerator.MoveNext())
+            {
+                var line = enumerator.Current;
+                if (string.IsNullOrEmpty(line)) { yield break; }
+
+                yield return line.AsSpan().ParseRangeValues<long>();
+            }
+        }
+
+        private IEnumerable<long> ForEachId()
+        {
+            while (enumerator.MoveNext())
+            {
+                var line = enumerator.Current;
+                if (long.TryParse(line.AsSpan(), out var id)) { yield return id; }
+            }
+        }
+    }
+
+    extension(ReadOnlySpan<char> range)
+    {
+        private NumericRange<T> ParseRangeValues<T>() where T : IBinaryInteger<T>
+        {
+            var dash = range.Trim().IndexOf('-');
+            return
+                dash <= 0 ||
+                !T.TryParse(range[..dash], null, out var first) ||
+                !T.TryParse(range[(dash + 1)..], null, out var last)
+                ? new(T.Zero, T.Zero) : new(first, last);
+        }
+    }
+
+    private static List<T> ToSortedList<T>(this IEnumerable<T> source) => source.ToList().SortList();
+
+    private static List<T> SortList<T>(this List<T> list) { list.Sort(); return list; }
+}
--- AOC25/Program.cs
+++ AOC25/Program.cs
@@ -5,11 +5,13 @@ using Day1 = AOC25.Day1;
 using Day2 = AOC25.Day2;
 using Day3 = AOC25.Day3;
 using Day4 = AOC25.Day4;
+using Day5 = AOC25.Day5;
 
 //SolveDay1();
 //SolveDay2();
 //SolveDay3();
-SolveDay4();
+//SolveDay4();
+SolveDay5();
 
 
 static void SolveDay1()
@@ -46,6 +48,16 @@ static void SolveDay4()
 {
     IPuzzleInput input = PuzzleInput.Create(4);
     var (part1, part2) = Day4.Puzzle.Solve(input);
+    Console.WriteLine($"""
+    Part 1:  {part1}
+    Part 2:  {part2}
+    """);
+}
+
+static void SolveDay5()
+{
+    IPuzzleInput input = PuzzleInput.Create(5);
+    var (part1, part2) = Day5.Puzzle.Solve(input);
     Console.WriteLine($"""
     Part 1:  {part1}
     Part 2:  {part2}
